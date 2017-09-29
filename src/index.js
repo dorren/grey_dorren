90db7255d2088c461df6f119487af8752c5a14e1
Dorren Chen <dorrenchen@gmail.com>
2017-09-29 00:24:57 -0400

    use redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import Board from './components/Board';
 import BoardReducer from './reducers/BoardReducer';
+
 import './index.css';
 
-const store = createStore(BoardReducer);
+//const store = createStore(BoardReducer);
+const store = createStore(BoardReducer, /* preloadedState, */ devToolsEnhancer(
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+));
 
 ReactDOM.render(
   <Provider store={store}>
